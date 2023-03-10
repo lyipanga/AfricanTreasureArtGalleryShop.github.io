d7f4615926f9f5f350b86af8183d29ddd39adee7
lyipanga <96458285+lyipanga@users.noreply.github.com>
2023-03-10 01:57:59 -0500

    added new file

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/1.JPEG";
+import product2 from "./assets/2.JPEG";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.JPG";
+import product5 from "./assets/5.JPG";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Photos",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Photos",
+    price: 299.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Photos",
+    price: 150.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Photos",
+    price: 99.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Photos",
+    price: 675.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Photos",
+    price: 799.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Photos",
+    price: 1299.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Photos",
+    price: 999.0,
+    productImage: product8,
+  },
+];
